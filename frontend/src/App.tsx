3b6adb170ef51247803e01f8bd491acc21d61e2c
priyash7 <priyashshah4@gmail.com>
2024-12-05 22:56:26 +0530

    completed login functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,22 @@ import { Body } from './components/Body';
 import { RecoilRoot } from 'recoil';
 import './App.css'
 import { Login } from './components/Login';
+import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom';
 
 function App() {
-
-  const data = localStorage.getItem('authorization');
 
   return (
     <>
       <RecoilRoot>
-        {data && <Body/>}
-        {!data && <Login/>}
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={localStorage.getItem('authorization') ? <Body/> : <Navigate to="/login" replace/>}
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
       </RecoilRoot>
     </>
   )
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,44 +1,43 @@
 import axios from "axios";
-import { Button } from "./Button"
+import { Button } from "./Button";
+import {useNavigate} from "react-router-dom";
 
 export const Login = () => {
 
-
+    const navigate = useNavigate();
     const handleLogin = async () => {
-
         const usernameElement = document.querySelector('input[name="username"]');
         const username = usernameElement ? (usernameElement as HTMLInputElement).value : '';
         const passwordElement = document.querySelector('input[name="password"]');
         const password = passwordElement ? (passwordElement as HTMLInputElement).value : '';
+
         try {
-            const result = await axios.post('http://localhost:8080/api/v1/signin',
-                {
-                    username : username,
-                    password : password
+            const result = await axios.post('http://localhost:8080/api/v1/signin', {
+                username: username,
+                password: password
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
                 },
-                {
-                headers : {
-                    'Content-Type' : 'application/json',
-                },
-            })
-            localStorage.setItem('authorization',result.data.token);
-        }catch (error){
+            });
+            localStorage.setItem('authorization', result.data.token);
+            return navigate('/')
+        } catch (error) {
             alert("Wrong password or username! Please try again!");
         }
+    };
 
-    }
-
-    return(
+    return (
         <>
             <div className="fixed inset-0 z-50 flex items-center justify-center">
                 <div className="h-[400px] w-[300px] bg-gray-200 flex flex-col items-center justify-center gap-3 p-4">
                     <h1>Enter username</h1>
-                    <input name='username' type='text' className="w-full mb-2 p-1 rounded-lg border border-gray-300"></input>
+                    <input name="username" type="text" className="w-full mb-2 p-1 rounded-lg border border-gray-300" />
                     <h1>Enter password</h1>
-                    <input name='password' type='password' className="w-full mb-2 p-1 rounded-lg border border-gray-300"></input>
-                    <Button variant="primary" size="md" onClick={() => {handleLogin()}} text={"Submit"} />
+                    <input name="password" type="password" className="w-full mb-2 p-1 rounded-lg border border-gray-300" />
+                    <Button variant="primary" size="md" onClick={()=>handleLogin()} text="Submit" />
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
